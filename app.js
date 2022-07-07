d43956b17e6ea0b737dd70a83683b8746760a30e
tqviet39 <tranquocviet39@gmail.com>
2022-07-07 21:41:47 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -12,5 +13,13 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, " Resource Not found"));
+});
 
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+        });
+});
 module.exports = app;
